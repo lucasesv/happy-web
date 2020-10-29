e6f94bcd1d0f4417bd1728e57d7023ba822e833c
Lucas Vasconcelos <lucas.vasconcelos@esig.com.br>
2020-10-28 23:20:04 -0300

    feat: add leaflet to display the maps
    Add the leaflet library to render the maps

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-
-import './styles/global.css';
+import 'leaflet/dist/leaflet.css';
 
 import Routes from './routes';
+import './styles/global.css';
 
 
 function App() {
