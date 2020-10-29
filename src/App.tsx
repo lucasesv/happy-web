6af701079591eebe2bdd33b19498ca083701e9f4
Lucas Vasconcelos <lucas.vasconcelos@esig.com.br>
2020-10-28 21:45:56 -0300

    feat: add routing support
    Add routing support, but there are no routes configured yet

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import './styles/global.css';
 
+import Routes from './routes';
+
 
 function App() {
   return (
-    <p>Happy Web Application</p>
+    <Routes />
   );
 }
 
